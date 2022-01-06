f439cbc0750a86e3f68402392c9f626fcc01972c
Brayam Corral Flores <brayamcorral@gmail.com>
2022-01-06 14:33:20 -0800

    modal

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import Setup from './tutorial/4-forms/setup/2-multiple-inputs';
+import Setup from './tutorial/6-useReducer/setup/index.js';
 
-// 7:06:25
+// 7:30:00
 function App() {
   return (
     <div className='container'>
