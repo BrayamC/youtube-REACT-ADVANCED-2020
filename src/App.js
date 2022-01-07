f56d8f0b2352e4f9480cfa5436fb4cd4f00c2114
Brayam Corral Flores <brayamcorral@gmail.com>
2022-01-07 13:20:21 -0800

    useContext

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Setup from './tutorial/6-useReducer/setup/index.js';
+import Setup from './tutorial/8-useContext/setup/1-context-api';
 
 // 7:30:00
 function App() {
